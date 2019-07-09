250f106d25df819bd40a65d0c0b310df1297a55f
rjLelis <renatojlelis@gmail.com>
2019-07-09 13:46:19 -0300

    Change variable name, app -> application

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
+application = Flask(__name__)
+CORS(application)
 
-@app.route('/')
+@application.route('/')
 def main():
     return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
